e280951ae6efbc67ee101390ea8df73e2f7b9609
mastomas12 <tomassasra12@gmail.com>
2025-02-11 22:07:35 +0700

    Commit Update

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,7 @@
-import { Pool } from "pg";
+import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
-  ssl: {
-    rejectUnauthorized: false, // Perlu untuk Neon
-  },
 });
 
 export default pool;
